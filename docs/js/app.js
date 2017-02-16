7fd508d0009b7d4291f57629a22f26be4492f495
arjun <arjun.bka@gmail.com>
2017-02-16 14:15:07 -0600

    integrated side fiddle to main

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,9 +1,22 @@
-var app = angular.module('myApp',[]);
+var app = angular.module('myApp', ['ngRoute', 'ngAnimate']);
 
 
-app.controller("HeaderCtrl", function($scope) {
-	$scope.appDetails = {
-		title: "BooKart",
-		tagline: "We have 1 million books for you"
-	};
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/portfolio", {
+            templateUrl: "/portifolio/docs/partials/book-list.html",
+            //controller: "graph"
+        })
+        .otherwise({
+            templateUrl: "/portifolio/docs/partials/header.html"
+        });
+
+});
+
+
+app.controller("HeaderCtrl", function ($scope) {
+    $scope.appDetails = {
+        title: "BooKart",
+        tagline: "We have 1 million books for you"
+    };
 });
