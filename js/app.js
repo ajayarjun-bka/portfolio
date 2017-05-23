fa7fffd97d4892fc1e6af7ad1cef27cadcc39c2e
Ajay Arjun <arjun.bka@gmail.com>
2017-05-23 12:17:48 -0400

    Fixed broken styles

--- js/app.js
+++ js/app.js
@@ -64,44 +64,41 @@ app.controller("projects", function ($scope) {
         "module to evaluate whether the children are performing the tasks correctly using Kinect camera.",
         technologies: "Python, Java Script, Java Servlets, Nao Qi Framework, Choregraphe 2.43, Unity Engine, Kinect Studio",
         github: "www"
-    }, {
-        title: "Web Platform for Navigating Hospital Admission Information",
-        description: "Java based web application which provides a user with useful information like the average length " +
-        "of stay, mortality rate and the aver-age cost of the treatment based on current admission records and diagnosis code",
-        technologies: "Java, Spring MVC, JPA, JSP",
-        github: "www"
     },
         {
             title: " Secure Cloud Storage",
-            description: "Cloud application developed using python that encrypt and uploads the file into IBM Bluemix" +
-            " storage container or SQL DB. It allows the user to download and Decrypt the file, list all the files and " +
-            "deleting specified file that is available in the container. Implemented separate containers and version " +
-            "control for multiple users.",
-            technologies: ": Python, AES Encryption",
+            description: "Cloud application developed using python that encrypts and uploads the file into IBM Bluemix " +
+            "storage container. It allows the user to download the decrypted file, list all the files and deleting " +
+            "specified file that is available in the container. Implemented separate containers and version control for each user.",
+            technologies: "Python, AES encryption, IBM Bluemix",
             github: "www"
         },
         {
-            title: " Database Caching",
-            description: "A Web interface application that allow the user to generate random queries to carry out " +
-            "analysis on Weather Data, improves the performance of SQL DB access time using memcache, developed using " +
-            "Flask-web framework and python implemented in Amazon AWS. It also compares the performance " +
-            "of SQL DB with and without cache.",
-            technologies: "Python, AWS, Flask",
+            title: " Database Caching using Elastic Storage",
+            description: "A web application connected to Amazon S3, that acts as a repository for historical weather " +
+            "data. The users were allowed generate random queries over the weather data to to view details for a city, " +
+            "sort the data, finding peak value, or by climatic condition. Implemented memcache to cache frequent " +
+            "queries by hashing. Reduced Database access by 10X times. ",
+            technologies: "Python, AWS Elastic Storage, S3, Python-flask",
             github: "www"
         },
 
         {
             title: " Hadoop Map Reduce and K-Means Clustering on Earthquake data set",
-            description: "Implemented Hadoop Map-reduce multi-node cluster on AWS to analyze earthquake patterns and " +
-            "used K-means clustering to visualize the results in the form of charts",
-            technologies: "Python, R, AWS MongoDB",
+            description: "Implemented Hadoop Map reduce multi-node cluster on Amazon Web Services to analyze earthquake " +
+            "data set for relationship between magnitude and depth, to identify if the magnitude is increasing over a " +
+            "fixed period. Performed K means clustering on the output from Map Reduce job to group data as per " +
+            "requirements and visualize it as scatter plots, histograms and bar graphs using R and shiny server. ",
+            technologies: "Python, R, AWS, S3",
             github: "www"
         },
         {
             title: "Static Code Analyzer for Security Analysis",
-            description: "JImplemented online static code analyzers [rats and Pylint] as REST API and displayed results " +
-            "dynamically with versioning and multiuser support.",
-            technologies: " Node.js, Angular JS, AWS, OAUTH 2.0, Passport, ExpressJs",
+            description: "Implemented online static code analyzer as REST APIs to analyze C/C++ and python code for " +
+            "common programming vulnerabilities. Generated reports of the results and stored a copy under respective " +
+            "users to track vulnerabilities. Implemented sanitization of inputs to avoid vulnerabilities like buffer " +
+            "overflow and cross site scripting. Performed stress testing using JMeter.",
+            technologies: "Node.js, Angularjs, Expressjs, AWS EC2",
             github: "www"
         },
         {
@@ -110,6 +107,14 @@ app.controller("projects", function ($scope) {
             "publicly, comment on pictures and set privacy. Used NO-SQL database to support performance and auto scaling " +
             "based on user load.Handles multiple user sessions and stress tested using JMeter.",
             technologies: "Google App Engine ,Python,Flask,MongoDB",
+            github: "www"
+        },
+        {
+            title: "K-NN with Cross Validation",
+            description: "Implemented K-NN using different distance measures such as Euclidean, Polynomial kernel and " +
+            "Radial basis kernel. It included preprocessing steps such as cleaning and normalization of data and 10-FCV " +
+            "to divide the data set into training and testing samples",
+            technologies: "Python",
             github: "www"
         }
 
