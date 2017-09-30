4bf8f22a8f2b2c38202fa2daea953da5e93bf68b
Ajay Arjun <arjun.bka@gmail.com>
2017-09-30 10:21:55 -0400

    updated contact details

--- js/app.js
+++ js/app.js
@@ -36,24 +36,22 @@ app.controller("intro", function ($scope) {
         headline: "Java Developer | Full Stack Developer",
         //headline: "",
         shortIntro: "Recently graduated with a Masters in Computer Science from The University of Texas at Arlington. " +
-        "I am passionate about Cloud Computing, Big Data and Web development." +
-        " I have developed full-stack (MEAN, React/Angular + Firebase, React/Angular " +
-        "+ Express) and client-side applications (React, Angular), applications. and have a strong grasp of object-oriented " +
-        "programming principles and design patterns.    ",
+        "I am passionate about Cloud Computing, and Big Data." +
+        " I have developed several cloud based applicaitons leveraging lambda function and microservices. Strong exposure in consuming and writing RESTFUl Api services. " +
+        "Created several skills for Amazon Alexa and published. I have a strong grasp of object-oriented programming principles and design patterns.",
         ps: "Kindly go through the ",
         ps1: "and",
         ps2: " sections to know more about me",
-        summary :["Over 3 years of experience in web application development using JAVA, J2EE, JDBC, Servlets, Spring, " +
-        "Hibernate, and JSP, HTML5, CSS/CSS3, JavaScript, AJAX, and JSON.","Strong in developing front-end for web applications " +
+        summary: ["Over 2 years of experience in web application development using JAVA, J2EE, JDBC, Servlets, Spring, " +
+        "Hibernate, and JSP, HTML5, CSS/CSS3, JavaScript, AJAX, and JSON.", "Strong in developing front-end for web applications " +
         "using JavaScript frameworks and libraries like Angular JS, Node.js, React.js, D3.js and other MVC frameworks.",
-            "Strong knowledge in React.js, testing react components using karma and mocha, and Webpack.","Experience in " +
+            "Strong knowledge in React.js, testing react components using karma and mocha, and Webpack.", "Experience in " +
             "all phases of Software Development Life Cycle (SDLC) methodologies like Agile/Scrum and Test-Driven Development (TDD).",
             "Experience with AWS services like EC2 instances, Elastic Beanstalk, S3, RDS, Load Balancing and Auto scaling.",
-            "Experience in creating Conceptual Data Models, Use Case Diagrams, Class Diagrams and Sequence Diagrams using UML with Rational Rose.",
-            "Experience with Databases like Oracle 10g, MYSQL, MongoDB.","Extensively used SQL in writing queries, database triggers, and stored " +
-            "procedures.","Experience with version control service GIT and error logging using Log4J.","Developed test " +
-            "scripts with JUnit and used JMeter for performance Testing.","Utilized FindBugs and PMD for implementing coding " +
-            "guidelines, code analyzer and code coverage.","Experience in working with build & deployment tools like Maven and Ant.",
+            "Experience with Databases like Oracle 10g, MYSQL, MongoDB.", "Extensively used SQL in writing queries, database triggers, and stored " +
+            "procedures.", "Experience with version control service GIT and error logging using Log4J.", "Developed test " +
+            "scripts with JUnit and used JMeter for performance Testing.", "Utilized FindBugs and PMD for implementing coding " +
+            "guidelines, code analyzer and code coverage.", "Experience in working with build & deployment tools like Maven and Ant.",
             "Experience in interacting with clients/users in gathering the user requirements."]
     };
 });
@@ -76,6 +74,15 @@ app.controller("projects", function ($scope) {
         technologies: "Python, Java Script, Java Servlets, Nao Qi Framework, Choregraphe 2.43, Unity Engine, Kinect Studio",
         github: "www"
     },
+        {
+            title: " Automated Notifications using Amazon SNS and SQS",
+            description: "Implemented automated message notification system using SNS and SQS on AWS using AWS JAVA SDK." +
+            " Wrote consumer and producers to consume and push messages into SQS. Created SNS topic which send out " +
+            "notifications to http/https endpoints. Added local signature verification to confirm the authenticity " +
+            "of the source of SNS messages.",
+            technologies: "Java, Eclipse, AWS SNS, SQS, AWS JAVA SDK, Tomcat",
+            github: "www"
+        },
         {
             title: " Secure Cloud Storage",
             description: "Cloud application developed using python that encrypts and uploads the file into IBM Bluemix " +
@@ -136,43 +143,56 @@ app.controller("contact", function ($scope) {
     $scope.info = {
         linkedin: "https://www.linkedin.com/in/ajayarjun/",
         github: "https://github.com/ajayarjun-bka",
-        email: "arjun.bka@gmail.com",
+        email: "ajayarjun.baalakrishnan@gmail.com",
         phone: "682-252-3332"
     }
 });
 
 app.controller("experience", function ($scope) {
     $scope.jobs = [{
-        company: "MS.SWAMINATHAN RESEACH FOUNDATION",
-        position: "WEB DEVELOPER INTERN",
-        period: "JAN 2014 - MAR 2014",
-        roles: ["Performed requirement analysis and developed a planning system using Java, to schedule conferences " +
-        "among branch research centers and training sessions to students to increase productivity.", "Carried out " +
-        "migration of website from PHP to Java environment."
+        company: "BIGPARSER",
+        position: "SOFTWARE DEVELOPER",
+        period: "JUN 2017 - PRESENT",
+        roles: ["Developed libraries in Java, Python and Nodejs to access Bigparser’s API’s.","Implemented AWS SNS for " +
+        "notifications through SMS, Emails and push notifications."," Leveraged AWS SQS to communicate among microservices" +
+        " using AWS JAVA SDK"," Developed Several Skills for Amazon Alexa"
         ]
-    }, {
-        company: "INFOSYS LIMITED, INDIA",
-        position: "SYSTEM ENGINEER",
-        period: "SEP 2014 - MAY 2015",
-        roles: ["Developed a web based application with SDLC (Software Development Life Cycle) model using Java, JSP, " +
-        "Servlets, Bootstrap and HTML to track internal resource allocation.","Experience in creating templates, components, " +
-        "workflows, widgets, user groups and web pages specific to the site as per the business requirement using Adobe CQ 5.6.",
-            "Performed client side design and validations using HTML/HTML5, jQuery, CSS/CSS3 and JavaScript.",
-            "Wrote SQL queries to retrieve and insert data from/into the database using Oracle 10g.","Used Hibernate as " +
-            "ORM to map java classes to data base tables.","Deployed the web application on Apache Tomcat application server.",
-            "Involved in code review and in Unit testing using JUnit and Integration testing of the application.",
-            "Tested web pages for CSS pixel-level layout matching, consistency with browser versions and platform independence.",
-            "Wrote media queries for making appropriate CSS fixes while making the pages responsive for desktop, tablet & mobile break points."]
-    }, {
-        company: "HERACLIA LAB, UTA",
-        position: "RESEARCH PROJECT",
-        period: "JAN 2016 - MAY 2016",
-        roles: ["Carried out a research on Attention-deficit/hyperactivity disorder with a team of # Members.",
-            "Carried out a pilot study and collected feedback from experts for the basis for research" +
-            "Built a interactive application with web interface to control a Humanoid robot which served as a medium to " +
-            "connect with children affected with ADHD", "Published the research proceedings to PETRA 2016."
+    },
+        {
+            company: "HERACLIA LAB, UTA",
+            position: "RESEARCH PROJECT",
+            period: "JAN 2016 - MAY 2016",
+            roles: ["Carried out a research on Attention-deficit/hyperactivity disorder with a team of # Members.",
+                "Carried out a pilot study and collected feedback from experts for the basis for research" +
+                "Built a interactive application with web interface to control a Humanoid robot which served as a medium to " +
+                "connect with children affected with ADHD", "Published the research proceedings to PETRA 2016."
+            ]
+        },
+        {
+            company: "INFOSYS LIMITED, INDIA",
+            position: "SYSTEM ENGINEER",
+            period: "SEP 2014 - MAY 2015",
+            roles: ["Developed a web based application with SDLC (Software Development Life Cycle) model using Java, JSP, " +
+            "Servlets, Bootstrap and HTML to track internal resource allocation.", "Experience in creating templates, components, " +
+            "workflows, widgets, user groups and web pages specific to the site as per the business requirement using Adobe CQ 5.6.",
+                "Performed client side design and validations using HTML/HTML5, jQuery, CSS/CSS3 and JavaScript.",
+                "Wrote SQL queries to retrieve and insert data from/into the database using Oracle 10g.", "Used Hibernate as " +
+                "ORM to map java classes to data base tables.", "Deployed the web application on Apache Tomcat application server.",
+                "Involved in code review and in Unit testing using JUnit and Integration testing of the application.",
+                "Tested web pages for CSS pixel-level layout matching, consistency with browser versions and platform independence.",
+                "Wrote media queries for making appropriate CSS fixes while making the pages responsive for desktop, tablet & mobile break points."]
+        },
+
+        {
+            company: "MS.SWAMINATHAN RESEACH FOUNDATION",
+            position: "WEB DEVELOPER INTERN",
+            period: "JAN 2014 - MAR 2014",
+            roles: ["Performed requirement analysis and developed a planning system using Java, to schedule conferences " +
+            "among branch research centers and training sessions to students to increase productivity.", "Carried out " +
+            "migration of website from PHP to Java environment."
+            ]
+        }
         ]
-    }]
 });
 
 // nav()
